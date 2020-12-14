ec5a01784dab7258991a804e23a392036be89290
kamal kafi <33871912+Kemo-Byte@users.noreply.github.com>
2020-12-14 07:05:23 +0200

    Update Database.php

--- app/models/Database.php
+++ app/models/Database.php
@@ -18,7 +18,7 @@
          {
               // use try to connect in database
               try {
-                   $conn = new PDO('mysql:host'.$this->host.';dbname='.$this->dbname), $this->dbuser, $dbpass, $this->options);
+                   $conn = new PDO('mysql:host'.$this->host.';dbname='.$this->dbname, $this->dbuser, $dbpass, $this->options);
               } catch (\PDOException $th) {
                    //throw Error if Not Connected
                    print "Faild To Connect into Database" . $th->getMessage();
