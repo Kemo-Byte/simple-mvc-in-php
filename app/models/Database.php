ce7cba3774cb0f5e911770ee4f7fe26a0e132b2d
kamal kafi <33871912+Kemo-Byte@users.noreply.github.com>
2020-12-14 06:45:33 +0200

    Update Database.php
    I just remove the comment and make the database connection more dynamic bro :)

--- app/models/Database.php
+++ app/models/Database.php
@@ -6,22 +6,19 @@
     class Database 
     {
          public $name;
-     //     protected $dsn = 'mysql:host=127.0.0.1';
-     //     protected $dbname = 'testing';
-     //     protected  $dbuser = 'root';
-     //     protected $dbpass = '';
-     //     protected $option = array(
-     //          PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
-     //     );
+         protected $host = 'localhost';
+         protected $dbname = 'testing';
+         protected  $dbuser = 'root';
+         protected $dbpass = '';
+         protected $options = array(
+              PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
+         );
 
          public function __construct()
          {
               // use try to connect in database
               try {
-                   $conn = new PDO('mysql:host=127.0.0.1;dbname=testing',
-                    'root', '', array(
-                         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'
-                    ));
+                   $conn = new PDO('mysql:host'.$this->host.';dbname='.$this->dbname), $this->dbuser, $dbpass, $this->options);
               } catch (\PDOException $th) {
                    //throw Error if Not Connected
                    print "Faild To Connect into Database" . $th->getMessage();
